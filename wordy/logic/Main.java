c88cf79987643969d846799317e0b7cb1853dfa4
Jose Guaro <jguaro@ucsd.edu>
2018-10-21 01:30:57 -0700

    Added compiler arguments and parsing

--- wordy/logic/Main.java
+++ wordy/logic/Main.java
@@ -1,54 +1,74 @@
 package wordy.logic;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import wordy.logic.compile.WordyCompiler;
 import wordy.logic.compile.structure.FileStructure;
 import wordy.logic.runtime.WordyRuntime;
+import wordy.logic.runtime.components.Instance;
 import wordy.logic.runtime.components.JavaInstance;
 
 public class Main {  
   
+  //short usage string for printing
+  private static final String USAGE = "Usage: wordy [options] <sources> [args]";
+  
   public static void main(String[] args) throws Exception {
-    String [] sourceFiles = { "src\\Sources\\Flood.w"};    
-           
-    printCompilerIntro(sourceFiles);
-    WordyCompiler compiler = new WordyCompiler(sourceFiles);
-    Map<String, FileStructure> fileMap = compiler.compile();
-     
-       
-    /*
-    System.out.println(Operator.simpleArithmetic(JavaInstance.wrapInstance(10), 
-                                                     JavaInstance.wrapInstance(10), 
-                                                     new Token("+", Type.PLUS, 0)).getInstance());
-    */
-    
-    /*
-    JavaInstance instance = JavaInstance.createStaticInstance(Main.class);
-    VariableMember variableMember = instance.retrieveVariable("changeMe");
-    variableMember.setValue(JavaInstance.wrapInstance(50));
-    System.out.println("RESULT: "+changeMe);
-    
-    
-    JavaInstance secondInstance = JavaInstance.createStaticInstance(Main.class);
-    VariableMember member = secondInstance.retrieveVariable("changeMe");
-    System.out.println("---RETRIEVE: "+ member.getValue());
-    
-    System.out.println(instance.retrieveVariable("changeMe").getValue());
-    instance.retrieveVariable("changeMe").setValue(JavaInstance.wrapInstance(10));
-    System.out.println("---RETRIEVE 2: "+ secondInstance.retrieveVariable("changeMe").getValue());
-    */
-    
-    
-    System.out.println("      --Form completed. Interpreting now...--      ");
-    WordyRuntime runtime = new WordyRuntime();
-    System.out.println("      --Initializing runtime environment--      ");
-    runtime.initialize(fileMap);
-    String mainSourceFile = "Flood";
-    System.out.println("      --Invoking main function in "+mainSourceFile+" --      ");
-    System.out.println();
-    runtime.execute("Flood", 1, JavaInstance.wrapInstance("hello"));
-    
+    if (args.length == 0) {
+      //no arguments. Print usage
+      printFullUsage(createOptions());
+    }
+    else {
+      ParsedArguments arguments = parseCompilerArgs(args);
+      if (arguments != null) {
+        //no error occurred while parsing arguments
+        String [] programArgs = arguments.arguments.toArray(new String[arguments.arguments.size()]);
+        String [] sourceFiles = arguments.sources.toArray(new String[arguments.sources.size()]);  
+        if (sourceFiles.length != 0) {
+          printCompilerIntro(sourceFiles);
+          
+          System.out.println("---SOURCES: "+Arrays.toString(sourceFiles));
+          
+          WordyCompiler compiler = new WordyCompiler(sourceFiles);
+          Map<String, FileStructure> fileMap = compiler.compile();
+          
+          
+          System.out.println("      --...Form completed. Interpreting now...--      ");
+          WordyRuntime runtime = new WordyRuntime();
+          System.out.println("      --Initializing runtime environment--      ");
+          runtime.initialize(fileMap);
+          String mainSourceFile = arguments.mainFile;
+          if (mainSourceFile == null) {
+            mainSourceFile = sourceFiles[0];
+          }
+          
+          System.out.println("      --Invoking main function in "+mainSourceFile+" --      ");
+          System.out.println();
+          
+          Instance [] progArgs = new Instance[programArgs.length];
+          for(int i = 0 ; i < progArgs.length; i++) {
+            progArgs[i] = JavaInstance.wrapInstance(programArgs[i]);
+          }
+          
+          runtime.execute(mainSourceFile, 1, JavaInstance.wrapInstance("hello"));
+        }
+        else {
+          //must have at least one source file
+          System.out.println(USAGE);
+        }
+      }
+    }  
   }
   
   private static void printCompilerIntro(String [] sources) {
@@ -57,7 +77,141 @@ public class Main {
     for(String x : sources) {
       System.out.println("* "+x);
     }
-    System.out.println("      FORMING SOURCES........");
+    System.out.println("........FORMING SOURCES........");
     System.out.println();
+  }
+  
+  /**
+   * Parses the arguments provided to the Wordy compiler
+   * @param args - the Strign arguments
+   * @return a ParsedArguments object holding all parsed information, or 
+   *         null if an error occurred while parsing
+   */
+  private static ParsedArguments parseCompilerArgs(String [] args) {
+    ParsedArguments arguments = new ParsedArguments();
+    Options options = createOptions();
+    try {
+      CommandLineParser parser = new DefaultParser();
+      CommandLine cmd = parser.parse( options, args);
+
+      for(Option proc : cmd.getOptions()) {
+        if (proc.getLongOpt().equals("help")) {
+          //print full sage and exit program
+          printFullUsage(options);
+          return null;
+        }
+        else if (proc.getLongOpt().equals("main")) {
+          //set given main source
+          arguments.setMainFile(proc.getValue());
+        }
+        else if (proc.getLongOpt().equals("class")) {
+          String directory = proc.getValue();
+          if (new File(directory).isDirectory() == false) {
+            System.err.println("'"+directory+"' isn't a directory.");
+            System.err.println(USAGE);
+            //return; DEV NOTE: uncomment later
+          }
+          arguments.setClassFolder(directory);
+        }
+      }
+      
+      String [] parsedArgs = cmd.getArgs();
+      
+      int index = 0;
+      while (index < parsedArgs.length) {
+        if (parsedArgs[index].endsWith(".w")) {
+          arguments.addSource(parsedArgs[index]);
+        }
+        else {
+          break;
+        }
+        index++;
+      }
+      
+      while (index < parsedArgs.length) {
+        if (parsedArgs[index].endsWith(".w")) {
+          System.err.println("Expected program arguments, not Wordy source files.");
+          System.out.println();
+          System.out.println(USAGE);
+          return null;
+        }
+        arguments.addArgument(parsedArgs[index]);
+        index++;
+      }
+    } catch (ParseException e) {
+      System.err.println(e.getMessage());
+      System.out.println();
+      printFullUsage(options);
+    }
+    
+    return arguments;
+  }
+  
+  /**
+   * Creates the Options object used for parsing command line options
+   * @return the Options object created
+   */
+  private static Options createOptions() {
+    Options options = new Options();
+    Option help = new Option("h", "prints the usage of this program, along with this message to stdout");
+    help.setLongOpt("help");
+    help.setArgs(0);
+    
+    Option mainFunc = new Option("m", "Sets which .w file contains the main function to execute"+System.lineSeparator()+
+                                      "By default, the left most .w file's main function will be executed, if it has one"+System.lineSeparator()+
+                                      "If not, an error will be thrown");
+    mainFunc.setLongOpt("main");
+    mainFunc.setArgs(1);
+    
+    Option compiledClasses = new Option("c", "Folder to search for .class files for type resolution");
+    compiledClasses.setArgs(1);
+    compiledClasses.setLongOpt("class");
+    
+    options.addOption(help);
+    options.addOption(mainFunc);
+    options.addOption(compiledClasses);
+    
+    return options;
+  }
+  
+  /**
+   * Prints the full usage message - includes short usage, but also options and their descriptions
+   * @param options - the Options object to use that holds all Option
+   */
+  private static void printFullUsage(Options options) {
+    System.out.println(USAGE);
+    for(Option ops : options.getOptions()) {
+      System.out.println("   -"+ops.getLongOpt()+" , -"+ops.getOpt()+"     "+ops.getDescription());
+    }
+  }
+  
+  public static class ParsedArguments{
+    private String mainFile;
+    private String classFolder; 
+    private List<String> sources;
+    private List<String> arguments;
+    
+    public ParsedArguments() {
+      sources = new ArrayList<>();
+      arguments = new ArrayList<>();
+    }
+    
+    public void setMainFile(String mainFile) {
+      this.mainFile = mainFile;
+    }
+    
+    public void setClassFolder(String classFolder) {
+      this.classFolder = classFolder;
+    }
+    
+    public void addArgument(String arg) {
+      arguments.add(arg);
+    }
+    
+    public void addSource(String source) {
+      if (sources.contains(source) == false) {
+        sources.add(source);
+      }
+    }
   }
 }
